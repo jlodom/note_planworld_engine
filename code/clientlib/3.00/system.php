61b244f7ec40b7ab05124c52742c8d62a413f69c
Johnnie Lamar Odom II <jlodom00@alumni.amherst.edu>
2018-06-11 17:51:01 -0500

    Forgot to move an updated system call for all user returns.

--- code/clientlib/3.00/system.php
+++ code/clientlib/3.00/system.php
@@ -7,9 +7,7 @@
 	Max version supported by server.
 	Arguments: None. Return: Version Float. */
 function versionGet ($arrayRestInputs) {
-
 	$versionReturn = '0';
-
 	if(file_exists($arrayRestInputs['enginebasedir'] . '/config.php')){
 		require_once($arrayRestInputs['enginebasedir'] . '/config.php');
 		if(defined('PW_VERSION')){
@@ -17,6 +15,22 @@ function versionGet ($arrayRestInputs) {
 		}
 	}
 	return $versionReturn;
+}
+
+function userslocalGet($arrayRestInputs){
+	$arrayUsers = array();
+	if((file_exists($arrayRestInputs['enginebasedir'] . '/lib/Planworld.php')) && (file_exists($arrayRestInputs['enginebasedir'] . '/lib/NodeToken.php')) ){
+		require_once($arrayRestInputs['enginebasedir'] . '/lib/NodeToken.php');
+		$objectToken = new NodeToken ();
+		if($objectToken->retrieveToken($arrayRestInputs['token'])){
+			if($objectToken->valid){
+				require_once($arrayRestInputs['enginebasedir'] . '/lib/Planworld.php');
+				$pw = new Planworld();
+				$arrayUsers = $pw->getAllUsers();
+			}
+		}
+	}
+	return $arrayUsers;
 }
 
 ?>
